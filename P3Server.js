09d19a3ca8b129e339ea23443c37d26063b96193
mkhan83 <khanmuneeb1028@gmail.com>
2017-07-17 23:29:03 -0400

    Updated File

--- P3Server.js
+++ P3Server.js
@@ -2,8 +2,8 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require ("express");
+var bodyParser = require ("body-parser");
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -11,13 +11,13 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT =  8081;
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
@@ -28,6 +28,16 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // LISTENER
 // The below code effectively "starts" our server
 // ==============================================================================
+
+app.get('/', function (req, res) {
+	console.log("Change showed here");
+   res.send('Welcome to The Scholarship Finder');
+})
+
+
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+

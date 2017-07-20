d152e0acf0c3eddd6956224eb927777eed02b515
mkhan83 <khanmuneeb1028@gmail.com>
2017-07-19 20:40:52 -0400

    change server

--- P3Server.js
+++ P3Server.js
@@ -11,13 +11,13 @@ var bodyParser = require ("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 // Sets an initial port. We"ll use this later in our listener
-var PORT =  8081;
+var PORT = 8081;
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
@@ -39,5 +39,7 @@ app.get('/', function (req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+app.listen(PORT, function(){
+console.log("Hello Muneeb")
 
-
+})
